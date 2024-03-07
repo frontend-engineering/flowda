61276288b1fd73ef395677280d5b9e21a10f57ed
shanehsi <shane.hsi@foxmail.com>
2024-03-07 19:32:51 +0800

    common deps

--- libs/schema/src/lib/schema.js
+++
@@ -1,3 +0,0 @@
-export function schema() {
-  return 'schema';
-}
--- libs/schema/src/lib/schema.spec.js
+++
@@ -1,6 +0,0 @@
-import { schema } from './schema';
-describe('schema', () => {
-  it('should work', () => {
-    expect(schema()).toEqual('schema');
-  });
-});
