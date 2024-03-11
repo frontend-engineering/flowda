6d8a0b15f20188cffc0cc0bed4d56453b0a4cbfe
shanehsi <shane.hsi@foxmail.com>
2024-03-11 16:58:13 +0800

    add storybook

---
+++ libs/theia-design/src/lib/theia-design.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta } from '@storybook/react';
+import {  TheiaDesign  }  from './theia-design';
+
+
+
+
+
+const Story: Meta<typeof TheiaDesign> = {
+  component: TheiaDesign,
+  title: 'TheiaDesign', 
+};
+export default Story;
+
+
+export const Primary = {
+  args: {
+  },
+};
