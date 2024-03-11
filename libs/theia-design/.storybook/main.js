5deff71e6cf47a576d44b6679f73f4825b771093
shanehsi <shane.hsi@foxmail.com>
2024-03-11 17:05:53 +0800

    update sb deps -> 7

--- libs/theia-design/.storybook/main.js
+++ libs/theia-design/.storybook/main.js
@@ -1,18 +1,16 @@
-
-
-
-
 module.exports = {
-  core: { builder: 'webpack5' },
-  stories: [
-    
-    '../src/lib/**/*.stories.mdx',
-    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'
-  ],
-  addons: ['@storybook/addon-essentials' , '@nrwl/react/plugins/storybook' 
-    , 'storybook-addon-swc' 
-    
-  ]
+    core: {builder: 'webpack5'},
+    stories: [
+
+        '../src/lib/**/*.stories.mdx',
+        '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'
+    ],
+    addons: [
+        '@storybook/addon-essentials',
+        '@nrwl/react/plugins/storybook',
+        'storybook-addon-swc'
+
+    ]
 };
 
 
