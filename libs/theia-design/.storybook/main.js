63851b004f6e5db504bab148c050c78ee6d8b93b
shanehsi <shane.hsi@foxmail.com>
2024-03-11 17:17:37 +0800

    update sb config -> 7

--- libs/theia-design/.storybook/main.js
+++ libs/theia-design/.storybook/main.js
@@ -1,10 +1,20 @@
+const path = require("path");
 module.exports = {
-    core: {builder: 'webpack5'},
+    core: {
+        disableTelemetry: true,
+    },
+
+    framework: {
+        name: '@storybook/react-webpack5',
+        options: {},
+    },
+
     stories: [
 
         '../src/lib/**/*.stories.mdx',
         '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'
     ],
+
     addons: [
         '@storybook/addon-essentials',
         '@nrwl/react/plugins/storybook',
