469a1d11c7aafb2bb38157d8e3c19f5c19b15b14
shanehsi <shane.hsi@foxmail.com>
2024-03-13 14:53:06 +0800

    generator can update symbols

--- libs/nx-plugin-flowda/src/generators/component/generator.ts
+++ libs/nx-plugin-flowda/src/generators/component/generator.ts
@@ -1,19 +1,49 @@
-import { generateFiles, getProjects, Tree } from '@nrwl/devkit'
+import { applyChangesToString, generateFiles, getProjects, Tree } from '@nrwl/devkit'
 import { componentGeneratorSchema } from './schema'
 import { z } from 'zod'
 import consola from 'consola'
 import * as path from 'path'
 import * as _ from 'radash'
+import { addImport } from '@nrwl/react/src/utils/ast-utils'
+import { ensureTypescript } from '@nrwl/js/src/utils/typescript/ensure-typescript'
 
-export default async function (tree: Tree, options: z.infer<typeof componentGeneratorSchema>) {
+let tsModule: typeof import('typescript')
+
+export default async function (host: Tree, options: z.infer<typeof componentGeneratorSchema>) {
+  if (!tsModule) {
+    tsModule = ensureTypescript()
+  }
+
   consola.info(`Create a ${options.name} component to design project `)
-  const project = getProjects(tree).get('design')
-  generateFiles(tree, path.join(__dirname, 'files'), path.join(project.sourceRoot, 'src/lib'), {
+  const designProject = getProjects(host).get('design')
+  const componentFileName = _.dash(options.name)
+  const modelSymbolName = _.pascal(options.name) + 'ModelSymbol'
+  const modelName = _.pascal(options.name) + 'Model'
+  generateFiles(host, path.join(__dirname, 'files'), path.join(designProject.sourceRoot, `lib/${componentFileName}`), {
     componentName: _.pascal(options.name),
-    componentFileName: _.dash(options.name),
-    modelName: _.pascal(options.name) + 'Model',
+    componentFileName: componentFileName,
+    modelName: modelName,
     modelFileName: _.dash(options.name) + '.model',
-    modelSymbolName: _.pascal(options.name) + 'ModelSymbol',
+    modelSymbolName: modelSymbolName,
     tmpl: '',
   })
+  const typesProject = getProjects(host).get('types')
+
+  const symbolsFilePath = path.join(typesProject.sourceRoot, 'lib/symbols.ts')
+  const symbolsSource = host.read(symbolsFilePath, 'utf-8')
+  const symbolsSourceFile = tsModule.createSourceFile(
+    symbolsFilePath,
+    symbolsSource,
+    tsModule.ScriptTarget.Latest,
+    true,
+  )
+  const changes = applyChangesToString(
+    symbolsSource,
+    addImport(
+      symbolsSourceFile,
+      `export const ${modelSymbolName} = Symbol.for('${modelName}')`,
+    ),
+  )
+  host.write(symbolsFilePath, changes)
+
 }
