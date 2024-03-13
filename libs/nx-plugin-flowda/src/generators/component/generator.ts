3dfb33dd35719742b05928cecb279d37a6806b80
shanehsi <shane.hsi@foxmail.com>
2024-03-13 15:33:48 +0800

    fix generator

--- libs/nx-plugin-flowda/src/generators/component/generator.ts
+++ libs/nx-plugin-flowda/src/generators/component/generator.ts
@@ -1,4 +1,4 @@
-import { applyChangesToString, generateFiles, getProjects, Tree } from '@nrwl/devkit'
+import { applyChangesToString, ChangeType, generateFiles, getProjects, StringChange, Tree } from '@nrwl/devkit'
 import { componentGeneratorSchema } from './schema'
 import { z } from 'zod'
 import consola from 'consola'
@@ -6,6 +6,9 @@ import * as path from 'path'
 import * as _ from 'radash'
 import { addImport } from '@nrwl/react/src/utils/ast-utils'
 import { ensureTypescript } from '@nrwl/js/src/utils/typescript/ensure-typescript'
+import type * as ts from 'typescript'
+import { findNodes } from 'nx/src/utils/typescript'
+import { insertImport } from '@nrwl/js'
 
 let tsModule: typeof import('typescript')
 
@@ -15,35 +18,116 @@ export default async function (host: Tree, options: z.infer<typeof componentGene
   }
 
   consola.info(`Create a ${options.name} component to design project `)
+
   const designProject = getProjects(host).get('design')
   const componentFileName = _.dash(options.name)
   const modelSymbolName = _.pascal(options.name) + 'ModelSymbol'
   const modelName = _.pascal(options.name) + 'Model'
+  const modelFileName = _.dash(options.name) + '.model'
+
   generateFiles(host, path.join(__dirname, 'files'), path.join(designProject.sourceRoot, `lib/${componentFileName}`), {
     componentName: _.pascal(options.name),
-    componentFileName: componentFileName,
-    modelName: modelName,
-    modelFileName: _.dash(options.name) + '.model',
-    modelSymbolName: modelSymbolName,
+    componentFileName,
+    modelName,
+    modelFileName,
+    modelSymbolName,
     tmpl: '',
   })
-  const typesProject = getProjects(host).get('types')
 
-  const symbolsFilePath = path.join(typesProject.sourceRoot, 'lib/symbols.ts')
-  const symbolsSource = host.read(symbolsFilePath, 'utf-8')
-  const symbolsSourceFile = tsModule.createSourceFile(
-    symbolsFilePath,
-    symbolsSource,
+  updateDesignModule(host, {
+    modelName, modelFileName, modelSymbolName, componentFileName,
+  })
+
+  addImportHelper(
+    host,
+    path.join(designProject.sourceRoot, 'index.ts'),
+    `export * from './lib/${componentFileName}/${componentFileName}'`,
+  )
+
+  const typesProject = getProjects(host).get('types')
+  addImportHelper(
+    host,
+    path.join(typesProject.sourceRoot, 'lib/symbols.ts'),
+    `export const ${modelSymbolName} = Symbol.for('${modelName}')`,
+  )
+}
+
+function addImportHelper(host: Tree, filePath: string, statement: string) {
+  const source = host.read(filePath, 'utf-8')
+  const sourceFile = tsModule.createSourceFile(
+    filePath,
+    source,
     tsModule.ScriptTarget.Latest,
     true,
   )
   const changes = applyChangesToString(
-    symbolsSource,
+    source,
     addImport(
-      symbolsSourceFile,
-      `export const ${modelSymbolName} = Symbol.for('${modelName}')`,
+      sourceFile,
+      statement,
     ),
   )
-  host.write(symbolsFilePath, changes)
+  host.write(filePath, changes)
+}
 
+export function addBind(
+  source: ts.SourceFile,
+  statement: string,
+) {
+  if (!tsModule) {
+    tsModule = ensureTypescript()
+  }
+  const bindDesignModuleVarDec = findNodes(source, tsModule.SyntaxKind.VariableDeclaration).filter((node: ts.Node) => {
+    if (tsModule.isVariableDeclaration(node)) {
+      const varName = node.name.getText()
+      return varName === 'bindDesignModule'
+    }
+  })
+  const bindExp = findNodes(bindDesignModuleVarDec[0], tsModule.SyntaxKind.ExpressionStatement)
+  const changes: StringChange[] = []
+
+  changes.push({
+    type: ChangeType.Insert,
+    index: bindExp[bindExp.length - 1].getEnd(),
+    text: `
+  ${statement}`,
+  })
+
+  return changes
+}
+
+function updateDesignModule(host: Tree, opt: {
+  componentFileName: string
+  modelName: string,
+  modelFileName: string
+  modelSymbolName: string
+}) {
+  const { componentFileName, modelName, modelFileName, modelSymbolName } = opt
+  const project = getProjects(host).get('design')
+  const filePath = path.join(project.sourceRoot, 'designModule.ts')
+  let source = host.read(filePath, 'utf-8')
+
+  let sourceFile = tsModule.createSourceFile(
+    filePath,
+    source,
+    tsModule.ScriptTarget.Latest,
+    true,
+  )
+  sourceFile = insertImport(host, sourceFile, filePath, modelSymbolName, '@flowda/types')
+  source = host.read(filePath, 'utf-8')
+
+  const changes = applyChangesToString(
+    source,
+    [
+      ...addImport(
+        sourceFile,
+        `import { ${modelName} } from './lib/${componentFileName}/${modelFileName}'`,
+      ),
+      ...addBind(
+        sourceFile,
+        `bind<${modelName}>(${modelSymbolName}).to(${modelName}).inSingletonScope()`,
+      ),
+    ],
+  )
+  host.write(filePath, changes)
 }
