ad04f87d69b1af4938950ee91485bc03b2ff87c2
shanehsi <shane.hsi@foxmail.com>
2024-03-13 14:34:32 +0800

    refactor generator

--- libs/nx-plugin-flowda/src/generators/component/generator.ts
+++ libs/nx-plugin-flowda/src/generators/component/generator.ts
@@ -1,20 +1,19 @@
-import { Tree } from '@nrwl/devkit'
+import { generateFiles, getProjects, Tree } from '@nrwl/devkit'
 import { componentGeneratorSchema } from './schema'
 import { z } from 'zod'
-import { componentGenerator, componentStoryGenerator } from '@nrwl/react'
 import consola from 'consola'
+import * as path from 'path'
+import * as _ from 'radash'
 
 export default async function (tree: Tree, options: z.infer<typeof componentGeneratorSchema>) {
   consola.info(`Create a ${options.name} component to design project `)
-
-  await componentGenerator(tree, {
-    name: options.name,
-    project: 'design',
-    style: 'css',
-    classComponent: true,
-  })
-  await componentStoryGenerator(tree, {
-    project: 'design',
-    componentPath: `lib/${options.name}/${options.name}.tsx`,
+  const project = getProjects(tree).get('design')
+  generateFiles(tree, path.join(__dirname, 'files'), path.join(project.sourceRoot, 'src/lib'), {
+    componentName: _.pascal(options.name),
+    componentFileName: _.dash(options.name),
+    modelName: _.pascal(options.name) + 'Model',
+    modelFileName: _.dash(options.name) + '.model',
+    modelSymbolName: _.pascal(options.name) + 'ModelSymbol',
+    tmpl: '',
   })
 }
