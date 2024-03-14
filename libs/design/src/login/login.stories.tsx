f27820139ab899853d52a58cd0bb7d685fc5857b
shanehsi <shane.hsi@foxmail.com>
2024-03-14 10:00:57 +0800

    sb use trpc

--- libs/design/src/login/login.stories.tsx
+++ libs/design/src/login/login.stories.tsx
@@ -1,24 +1,49 @@
 import 'reflect-metadata'
-import type { Meta } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 import { Login } from './login'
 import { Container } from 'inversify'
 import { designModule } from '../designModule'
 import { LoginModelSymbol } from '@flowda/types'
 import { LoginModel } from './login.model'
+import '@elastic/eui/dist/eui_theme_light.css'
+import '../reset.css'
+import React from 'react'
+import { observer } from 'mobx-react'
+import { trpc } from '../../stories/trpc/trpc-client'
 
 const container = new Container()
 container.load(designModule)
+const loginModel = container.get<LoginModel>(LoginModelSymbol)
+loginModel.handlers.validate = (input) => trpc.hello.validate.mutate(input)
+loginModel.checkLogin()
 
-const Story: Meta<typeof Login> = {
-  component: Login,
-  title: 'Login',
+@observer
+class LoginWrapper extends React.Component<{
+  model: LoginModel
+}> {
+  render() {
+    const model = this.props.model
+    return (
+      <>
+        <Login model={model}/>
+        {model.isLogin ? (
+          <button onClick={() => model.logout()}>logout</button>
+        ) : (
+          <button onClick={() => model.login()}>Login</button>
+        )}
+      </>
+    )
+  }
 }
-export default Story
 
-const model = container.get<LoginModel>(LoginModelSymbol)
+// name convention https://storybook.js.org/docs/writing-stories/args#story-args
+const meta: Meta<typeof LoginWrapper> = {
+  component: LoginWrapper,
+}
+export default meta
 
-export const Primary = {
+export const Primary: StoryObj<typeof LoginWrapper> = {
   args: {
-    model,
+    model: loginModel,
   },
 }
