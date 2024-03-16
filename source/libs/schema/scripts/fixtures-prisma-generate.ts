69dff468c10dcbf6363e08fc7d16c8a0f5b53cf0
shanehsi <shane.hsi@foxmail.com>
2024-03-16 16:43:50 +0800

    build: done fixtures generate script

--- source/libs/schema/scripts/fixtures-prisma-generate.ts
+++ source/libs/schema/scripts/fixtures-prisma-generate.ts
@@ -1,9 +1,25 @@
 import * as fs from 'fs-extra'
 import * as path from 'path'
+import consola from 'consola'
+import { execSync } from 'child_process'
+
+const FIXTURES_DIR = path.join(__dirname, '../src/parser/prisma-zod/__fixtures__')
 
 async function run() {
-  const dirs = await fs.readdir(path.join(__dirname, '../src/parser/prisma-zod/__fixtures__'))
-  console.log(dirs)
+  const dirs = (await fs.readdir(FIXTURES_DIR, {
+    withFileTypes: true,
+  }))
+    .filter(dir => dir.name.startsWith('prisma-'))
+    .map(dir => dir.name)
+
+  for (const dir of dirs) {
+    consola.start(`run ${dir}`)
+    execSync(`npm run generate`, {
+      cwd: path.join(FIXTURES_DIR, dir),
+      stdio: 'inherit',
+    })
+    consola.success(`run ${dir}`)
+  }
 }
 
 void run()
