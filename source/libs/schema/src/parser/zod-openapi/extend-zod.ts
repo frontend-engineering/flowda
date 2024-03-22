45852398df6f39a1f4d45dab353c133b0984e3ee
shanehsi <shane.hsi@foxmail.com>
2024-03-22 16:54:02 +0800

    build: patch zod-api, comment out declare zod to avoid interface merge

--- source/libs/schema/src/parser/zod-openapi/extend-zod.ts
+++ source/libs/schema/src/parser/zod-openapi/extend-zod.ts
@@ -1,6 +1,13 @@
-import { z } from 'zod'
+import { z, ZodTypeAny, ZodTypeDef } from 'zod'
 import { extendZodWithOpenApi } from '@anatine/zod-openapi'
+import { ExtendSchemaObject } from '@flowda/types'
 
 export function extendZod(zod: typeof z, forceOverride = false) {
   extendZodWithOpenApi(zod, forceOverride)
+}
+
+declare module 'zod' {
+  interface ZodType<Output, Def extends ZodTypeDef, Input = Output> {
+    openapi<T extends ZodTypeAny>(this: T, metadata: Partial<ExtendSchemaObject>): T;
+  }
 }
