5cd05b75fe8446af79df34d24cabf2ac6ed2deeb
shanehsi <shane.hsi@foxmail.com>
2024-03-29 22:14:42 +0800

    test: prisma output should not end with @prisma/client, or runtime will be empty

--- source/libs/schema/src/parser/prisma-zod/__fixtures__/generated/prisma-01/zod/index.ts
+++ source/libs/schema/src/parser/prisma-zod/__fixtures__/generated/prisma-01/zod/index.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { Prisma } from '../@prisma/client';
+import { Prisma } from '../@prisma/client-01';
 import { extendZod } from '../../../../../zod-openapi/extend-zod';
 extendZod(z);
 
