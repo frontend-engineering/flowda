646889a119d08f74bfc940ba5d9805376a2f7e74
shanehsi <shane.hsi@foxmail.com>
2024-03-29 15:46:11 +0800

    build: sh

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -11,6 +11,18 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 cd ../source
 
+echo -e "${YELLOW}pnpm i...${RESET}\n"
+pnpm dlx pnpm@7.33.7 i --frozen-lockfile
+echo -e "${GREEN}pnpm i done.${RESET}\n"
+
+echo -e "${YELLOW}yalc update...${RESET}\n"
+yalc update
+echo -e "${GREEN}yalc update done.${RESET}\n"
+
+echo -e "${YELLOW}nx reset...${RESET}\n"
+./node_modules/.bin/nx reset
+echo -e "${GREEN}nx reset done.${RESET}\n"
+
 echo -e "${YELLOW}lint all...${RESET}\n"
 NX_DAEMON=false ./node_modules/.bin/nx run-many --target lint
 echo -e "${GREEN}lint done.${RESET}\n"
--- scripts/types-dev.sh
+++ scripts/types-dev.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 cd ../source
 
-NX_DAEMON=false ./node_modules/.bin/nx run types:dev
+./node_modules/.bin/nx run types:dev
