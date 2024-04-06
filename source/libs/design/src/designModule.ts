a6208122a6b4350379af92e4515abd537ad214b7
shanehsi <shane.hsi@foxmail.com>
2024-04-06 09:57:32 +0800

    Shanehsi/floss 27 theme first optimize (#15)
    * feat: a simple func to generate theia theme json from linear theme
    
    * feat: eui listen theme change
    feat: ag style optmize

--- source/libs/design/src/designModule.ts
+++ source/libs/design/src/designModule.ts
@@ -1,16 +1,18 @@
 import { ContainerModule, interfaces } from 'inversify'
 import { LoginModel } from './login/login.model'
-import { LoginModelSymbol, PreviewModelSymbol, GridModelSymbol } from '@flowda/types'
+import { LoginModelSymbol, PreviewModelSymbol, GridModelSymbol, ThemeModelSymbol } from '@flowda/types'
 
 import { PreviewModel } from './preview/preview.model'
 
 import { GridModel } from './grid/grid.model'
+import { ThemeModel } from './theme/theme.model'
 
 export const designModule = new ContainerModule(bind => {
   bindDesignModule(bind)
 })
 
 export const bindDesignModule = (bind: interfaces.Bind) => {
+  bind<ThemeModel>(ThemeModelSymbol).to(ThemeModel).inSingletonScope()
   bind<LoginModel>(LoginModelSymbol).to(LoginModel).inSingletonScope()
   bind<PreviewModel>(PreviewModelSymbol).to(PreviewModel).inSingletonScope()
   bind<GridModel>(GridModelSymbol).to(GridModel).inSingletonScope()
--- source/libs/design/src/index.ts
+++ source/libs/design/src/index.ts
@@ -3,3 +3,4 @@ export * from './grid/grid'
 export * from './grid/grid.model'
 export * from './login/login';
 export * from './login/login.model';
+export * from './theme/theme.model';
