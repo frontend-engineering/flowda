dc47cc184fe8a5dffbbeccaac5bc5f5a5c0b6d96
shanehsi <shane.hsi@foxmail.com>
2024-04-07 22:03:59 +0800

    build: dev.sh support only source

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+source="$1"
 
 cat << EOF
 
 This script first \`nx reset\` then run these scripts in parallel:
-1. ./zod-openapi-dev.sh
-2. ./zod-prisma-types-dev.sh
+1. ./zod-openapi-dev.sh (not s)
+2. ./zod-prisma-types-dev.sh (not s)
 3. types:dev
 4. schema:dev
 5. nx-plugin:dev
@@ -36,11 +37,21 @@ cd $SCRIPT_DIR
 
 sh ./nx-reset.sh
 
-concurrently --restart-tries -1 --restart-after 10000 -c "auto" \
-  -n openapi,prisma,types,schema,nx-plugin,design \
-  "./zod-openapi-dev.sh" \
-  "./zod-prisma-types-dev.sh" \
-  "./run.sh types dev" \
-  "./run.sh schema dev" \
-  "./run.sh nx-plugin dev" \
-  "./run.sh design dev"
+if [ "$source" = "s" ]
+then
+  concurrently --restart-tries -1 --restart-after 10000 -c "auto" \
+    -n types,schema,nx-plugin,design \
+    "./run.sh types dev" \
+    "./run.sh schema dev" \
+    "./run.sh nx-plugin dev" \
+    "./run.sh design dev"
+else
+  concurrently --restart-tries -1 --restart-after 10000 -c "auto" \
+    -n openapi,prisma,types,schema,nx-plugin,design \
+    "./zod-openapi-dev.sh" \
+    "./zod-prisma-types-dev.sh" \
+    "./run.sh types dev" \
+    "./run.sh schema dev" \
+    "./run.sh nx-plugin dev" \
+    "./run.sh design dev"
+fi
