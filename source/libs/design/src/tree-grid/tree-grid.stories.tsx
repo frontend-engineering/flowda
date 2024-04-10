5ea6ec24f798eb5859cb2814709567ab3a03bbb8
shanehsi <shane.hsi@foxmail.com>
2024-04-10 17:28:54 +0800

    story: fix

--- source/libs/design/src/tree-grid/tree-grid.stories.tsx
+++ source/libs/design/src/tree-grid/tree-grid.stories.tsx
@@ -6,14 +6,24 @@ import 'ag-grid-community/styles/ag-theme-quartz.css'
 import type { Meta, StoryObj } from '@storybook/react'
 import { TreeGrid } from './tree-grid'
 import { Container } from 'inversify'
-import { TreeGridModelSymbol } from '@flowda/types'
+import { GridModelSymbol, TreeGridModelSymbol } from '@flowda/types'
 import { designModule } from '../designModule'
 import { TreeGridModel } from './tree-grid.model'
 import { GridWrapper } from '../../stories/grid-wrapper'
-import { getData } from './__stories__/getData'
+import { GridModel } from '../grid/grid.model'
+import { trpc } from '../../stories/trpc/trpc-client'
 
 const container = new Container()
 container.load(designModule)
+container.rebind<GridModel>(GridModelSymbol).to(GridModel).inRequestScope()
+  .onActivation(({ container }, gridModel) => {
+    gridModel.handlers.onContextMenu = (e) => {
+      console.log(e)
+    }
+    gridModel.apis.getResourceData = (input) => trpc.hello.getResourceData.query(input)
+    gridModel.apis.putResourceData = (input) => trpc.hello.putResourceData.mutate(input)
+    return gridModel
+  })
 
 const meta: Meta<typeof GridWrapper> = {
   component: GridWrapper,
@@ -22,11 +32,14 @@ const meta: Meta<typeof GridWrapper> = {
 
 export default meta
 
-const model = container.get<TreeGridModel>(TreeGridModelSymbol)
-model.rowData = getData()
-
+const gridModel = container.get<GridModel>(GridModelSymbol)
+const treeGridModel = container.get<TreeGridModel>(TreeGridModelSymbol)
+const treeGridUri = 'tree-grid://flowda?schemaName%3DMenuResourceSchema%26displayName%3D%E8%8F%9C%E5%8D%95%233%3AtreeData%26id%3D3%26field%3DtreeData'
+treeGridModel.resetGridReadyPromise(treeGridUri)
+treeGridModel.setGridModel(gridModel)
+treeGridModel.loadData()
 export const Primary: StoryObj<typeof GridWrapper> = {
   args: {
-    children: <TreeGrid model={model} />,
+    children: <TreeGrid model={treeGridModel} />,
   },
 }
