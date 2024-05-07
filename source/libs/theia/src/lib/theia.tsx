c4d6ef430053bd36042eef99fd8f3bb29e43da36
shanehsi <shane.hsi@foxmail.com>
2024-05-07 14:50:17 +0800

    feat(theia): add a empty react lib, will mv theia things here, or storybook will failed due to theia deps

---
+++ source/libs/theia/src/lib/theia.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface TheiaProps {}
+
+export function Theia(props: TheiaProps) {
+  return (
+    <div>
+      <h1>Welcome to Theia!</h1>
+    </div>
+  )
+}
+
+export default Theia
